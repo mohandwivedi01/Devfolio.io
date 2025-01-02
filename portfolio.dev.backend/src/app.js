be3beb23d1ee3fab027b6970a3d042fc3ffd3156
mohandwivedi01 <manmohandwivedi@yahoo.com>
2025-01-02 21:29:50 +0530

    add middleware for JSON parsing, URL encoding, static files, and cookie parsing; implement async error handling

--- portfolio.dev.backend/src/app.js
+++ portfolio.dev.backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 
 const app = express();
@@ -10,6 +11,13 @@ app.use(cors({
     credentials:true
 }))
 
+app.use(express.json({limit:"16kb"}))
+
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
