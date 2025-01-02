bc228ffc8b6601d2ae4b2491b644611044326896
mohandwivedi01 <manmohandwivedi@yahoo.com>
2025-01-02 16:57:35 +0530

    initialise project setup frontend and project files

---
+++ portfolio.dev/src/App.jsx
@@ -0,0 +1,12 @@
+import Dev from "./dev.jsx";
+function App() {
+  return (
+    <>
+      <h1>Hello Vite & React java</h1>
+      <Dev/> 
+    </>
+  )
+}
+
+export default App
+ 
---
+++ portfolio.dev/src/dev.jsx
@@ -0,0 +1,7 @@
+function Dev(){
+    return(
+        <h3>we make dev docs easy</h3>
+    )
+}
+
+export default Dev;
