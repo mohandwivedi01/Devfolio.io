9412e55ff0b5f2def2fe1138d1c3ddf47083655c
mohandwivedi01 <manmohandwivedi@yahoo.com>
2025-01-08 23:31:14 +0530

    refactor blog controller; remove unused user model import

--- portfolio.dev.backend/src/controllers/blog.controller.js
+++ portfolio.dev.backend/src/controllers/blog.controller.js
@@ -1,5 +1,4 @@
 import {Blog} from '../models/blog.model.js';
-import {User} from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
